bd10422ac278c60b1beef86f05b536bd92bba8a1
TimKapunkt <96684814+TimKapunkt@users.noreply.github.com>
2022-01-02 15:17:56 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
 input.onButtonPressed(Button.A, function () {
+    aWurdeGedrueckt = 1
     basic.showIcon(IconNames.Heart)
     basic.showString("" + (input.temperature()))
 })
+let aWurdeGedrueckt = 0
+aWurdeGedrueckt = 0
 basic.showArrow(ArrowNames.West)
+basic.forever(function () {
+    if (aWurdeGedrueckt == 1) {
+        basic.showString("" + (input.temperature()))
+    }
+})
